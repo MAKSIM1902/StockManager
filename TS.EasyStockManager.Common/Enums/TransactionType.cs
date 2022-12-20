dbf4cd166a9f9c907b3061921b6e3fbbe2744189
m.shpakovskiy@softteco.com <m.shpakovskiy@softteco.com>
2022-12-21 00:46:40 +0300

    init

---
+++ TS.EasyStockManager.Common/Enums/TransactionType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TS.EasyStockManager.Common.Enums
+{
+    public enum TransactionType
+    {
+        [Display(Name = "Получение Товара")]
+        StockIn = 1,
+        [Display(Name = "Нет в Наличии")]
+        StockOut = 2,
+        [Display(Name = "Отгрузка")]
+        Transfer = 3
+    }
+}
